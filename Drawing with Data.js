5092e7a34d3fc041296d2ef0bf1d35e90c78ae43
HYPERTONE <42849765+HYPERTONE@users.noreply.github.com>
2019-10-31 14:23:13 -0400

    Update Drawing with Data.js

--- Drawing with Data.js
+++ Drawing with Data.js
@@ -1,16 +1,17 @@
 
-// Scalable Vector Graphs (SVG)
+// [Scalable Vector Graphs (SVG)]
 
 // - Text (XML) based image format
 // - Can draw rect, circle, ellipse, line, text
 
 
 // All SVG properties are specified as attributes:
-
 <element property="value"></element>
 
+//////////////////////////////
+// <!-- Create SVG Element --> Circle
+//////////////////////////////
 
-<!-- Create SVG Element -->
 
 // Width and Height
 var w = 500; 
@@ -41,12 +42,40 @@ circles.attr("cx", function(d, i) {   // cx is the x-position value of the cente
         });
 
 
+// Add some color
+circles.attr("fill", "yellow")
+       .attr("stroke", "orange")
+       .attr("stroke-width", function(d) {
+             return d/2;
+        });
 
 
 
 
+//////////////////////////////
+// <!-- Create SVG Element --> Rect
+//////////////////////////////
 
+var dataset = [ 5, 10, 13, 19, 21, 25, 22, 18, 15, 13,
+               11, 12, 15, 20, 18, 17, 16, 18, 23, 25 ];
 
+// Width and Height
+var w = 500; 
+var h = 50;
 
+var svg = d3.select("body")         // This inserts a new <svg> element just before closing the </body> tag,
+            .append("svg")          // and assigns the given width/height.
+            .attr("width", w)
+            .attr("height", h);
 
+svg.selectAll("rect")
+    .data(dataset)
+    .enter()
+    .append("rect")
+    .attr("x", function(d, i) {
+            return i * 21;
+     })                             // x location
+    .attr("y", 0)                   // y location
+    .attr("width", 20)              // width of rect
+    .attr("height", 100);           // height of rect
 
