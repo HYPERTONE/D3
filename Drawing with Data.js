483e8322b7e193a0a724fc7b95e339fe8f58f19e
HYPERTONE <42849765+HYPERTONE@users.noreply.github.com>
2019-11-01 07:48:25 -0400

    Update Drawing with Data.js

--- Drawing with Data.js
+++ Drawing with Data.js
@@ -61,21 +61,61 @@ var dataset = [ 5, 10, 13, 19, 21, 25, 22, 18, 15, 13,
 
 // Width and Height
 var w = 500; 
-var h = 50;
+var h = 100;
+var barPadding = 1;
 
 var svg = d3.select("body")         // This inserts a new <svg> element just before closing the </body> tag,
             .append("svg")          // and assigns the given width/height.
-            .attr("width", w)
+            .attr("width", w / dataset.length - barPadding)
             .attr("height", h);
 
 svg.selectAll("rect")
     .data(dataset)
     .enter()
     .append("rect")
-    .attr("x", function(d, i) {
-            return i * 21;
-     })                             // x location
-    .attr("y", 0)                   // y location
-    .attr("width", 20)              // width of rect
-    .attr("height", 100);           // height of rect
+    .attr("x", function(d, i) {				// x location
+		return i * (w / dataset.length); 
+	})                   
+    .attr("y", function(d) {				// y location
+		return h - (d * 4);
+	})                   
+    .attr("width", w / dataset.length - barPadding)                     // width of rect
+    .attr("height", function(d) {
+		return d * 4; 										// height of rect
+	})
+	.attr("fill", function(d) {
+		return "rgb(0, 0, " + Math.round(d * 10) + ")";
+	});
+	
+	
+svg.selectAll("text")					// add text labels to plot
+   .data(dataset)
+   .enter()
+   .append("text")
+   .text(function(d) {
+		return d;
+   })
+   .attr("x", function(d, i) {				// position label values
+		return i * (w / dataset.length) + 		// set the x-pos to the left edge of
+		(w / dataset.length - barPadding) / 2;	// each bar plus half the bar width
+   })
+   .attr("y", function(d) {
+		return h - (d * 4) + 14;		// offset them so they're even
+   })
+   .attr("font-family", "sans-serif")
+   .attr("font-size", "11px")				// set font size and color
+   .attr("fill", "white")
+   .attr("text-anchor", "middle");				// anchor text
+           
+
+
+
+
+
+
+
+
+
+
+
 
