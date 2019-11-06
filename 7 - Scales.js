d2f77cf55d106567de100a105888b6c905f9733e
HYPERTONE <42849765+HYPERTONE@users.noreply.github.com>
2019-11-06 09:16:12 -0500

    Update 7 - Scales.js

--- 7 - Scales.js
+++ 7 - Scales.js
@@ -6,34 +6,64 @@
 // A scale's output range is the range of possible output values, commonly used as display values in pixel units.
 
 
-
-// Creating a scale
-
-var scale = d3.scaleLinear();
-
-scale.domain([100, 500])    // Set scale input domain
-scale.range([10, 350])      // Set output range
+----------
+// 1. Set height, width, padding
+// 2. Create scales
+// 3. Create SVG element
+// 4. Add labels
 
 
-// Example
-
-
-var w = 500;  // width, height, padding
+// Height, width, padding
+var w = 500;  
 var h = 300;
 var padding = 20;
 
 var dataset = [
-							[5, 20], [480, 90], [250, 50], [100, 33], [330, 95],
-							[410, 12], [475, 44], [25, 67], [85, 21], [220, 88], [600, 150]
-						  ];
+		[5, 20], [480, 90], [250, 50], [100, 33], [330, 95],
+		[410, 12], [475, 44], [25, 67], [85, 21], [220, 88]
+	      ];
 
+// Create Scales
 var xScale = d3.scaleLinear()
                 .domain([0, d3.max(dataset, function(d) { return d[0]; })])
                 .range([0, w]);			// or if we wanted edge padding: .range([padding, w - padding * 2]);
 
 var yScale = d3.scaleLinear()             
 		.domain([0, d3.max(dataset, function (d) { return d[1]; })])
-		.range([0, h]); 
+		.range([0, h]); 		// or if we wanted invert/edge padding: .range([h - padding, padding]);
+
+var rScale = d3.scaleLinear()
+	    	.domain([0, d3.max(dataset, function(d) { return d[1]; })])
+	        .range([2, 5]);
+
+// Create SVG element
+
+var svg = d3.select("body")	// This inserts a new <svg> element just before closing the </body> tag,
+	    .append("svg")	// and assigns the given width/height.
+	    .attr("width", w + padding)
+	    .attr("height", h + padding)
+
+svg.selectAll("circle")		// take all the datapoints and structure them
+	    .data(dataset)	// data() binds our data to the elements we're about to create
+	    .enter()		// enter() returns a placeholder reference to the new element
+            .append("circle")	// append circles to the end of the SVG element
+	    .attr("cx", function(d) { return xScale(d[0]); }) // return the scaled element
+	    .attr("cy", function(d) { return yScale(d[1]); }) 
+	    .attr("r", function(d) { return rScale(d[1]); }) 
+
+
+// Add Labels
+svg.selectAll("text")
+		.data(dataset)
+		.enter()
+		.append("text")
+		.text(function(d) { return d[0] + "," + d[1]; }) // specify the text elements contents
+		.attr("x", function(d) { return xScale(d[0]); }) // specify text position with scale
+		.attr("y", function(d) { return yScale(d[0]); }) 
+		.attr("r", function(d) { return rScale(d[0]); })
+		
+
+
 
 
 
