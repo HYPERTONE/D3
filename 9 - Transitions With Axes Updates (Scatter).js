c8336a06ea8518a36362d82d14cbe66c688f513f
HYPERTONE <42849765+HYPERTONE@users.noreply.github.com>
2019-11-08 08:13:38 -0500

    Create 9 - Transitions With Axes Updates (Scatter).js

---
+++ 9 - Transitions With Axes Updates (Scatter).js
@@ -0,0 +1,14 @@
+// ---------- 
+// ---------- Transitions with axes update
+// ---------- 
+
+// 1. Declare height, width, padding
+// 2. Construct Scale
+// 3. Construct Axis/Axes
+// 4. Create SVG Element (and bars)
+// 5. Create Labels
+// 6. Create Axis/Axes
+
+// 1. Add an event listener
+// 2. Update data, scale, all rects, all labels
+
